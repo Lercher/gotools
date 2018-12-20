a4e9c347ccdc305085c2f6a1eb3ab9f634f9ce59
LERCHER Martin <martin.lercher@soprabanking.com>
2018-12-20 23:41:27 +0100

    hopalong: colorfull images and image size parameters

--- hopalong/hopimage.go
+++ hopalong/hopimage.go
@@ -5,25 +5,35 @@ import (
 	"image/color"
 	"image/png"
 	"io"
+
+	"github.com/usedbytes/hsv"
 )
 
-func hopPNG(w io.Writer, width, height, rounds int) {
-	img := hopimg(width, height, rounds)
+func hopPNG(w io.Writer, width, height, rounds, nextColor int) {
+	img := hopimg(width, height, rounds, nextColor)
 	png.Encode(w, img)
 }
 
-func hopimg(width, height, rounds int) *image.NRGBA {
+func hopimg(width, height, rounds, nextColor int) *image.NRGBA {
 	h := &hop{}
 	h.randomizeABC()
 	h.bounds(rounds)
 	t := h.transform(width, height)
 	h.reset()
 
-	c := color.RGBA{255, 30, 10, 255}
 	rect := image.Rect(0, 0, width, height)
 	img := image.NewNRGBA(rect)
 
+	n := nextColor + 1
+	ci := 0
+	var c color.Color
 	h.rounds(rounds, func() {
+		n++
+		if n >= nextColor {
+			n = 0
+			ci++
+			c = hsv.HSVColor{H: uint16(ci % 360), S: 255, V: 255}
+		}
 		x, y := t.tr(h.x, h.y)
 		img.Set(x, y, c)
 	})
--- hopalong/main.go
+++ hopalong/main.go
@@ -3,18 +3,27 @@ package main
 // go build && ./hopalong >x.png && rm hopalong
 
 import (
+	"flag"
 	"log"
 	"os"
 	"time"
 )
 
+var (
+	flagWidth     = flag.Int("w", 1920, "width of the png image")
+	flagHeight    = flag.Int("h", 1920, "height of the png image")
+	flagRounds    = flag.Int("n", 5000000, "number of plotted orbit pixels")
+	flagNextColor = flag.Int("c", 1000, "change color every this number of iterations")
+)
+
 func main() {
 	log.Println("This is HopAlong, (c) 2018 by Martin Lercher")
+	flag.Parse()
 
 	t := time.Now()
 	defer func() {
 		log.Println("runtime:", time.Now().Sub(t))
 	}()
 
-	hopPNG(os.Stdout, 1920, 1080, 100000)
+	hopPNG(os.Stdout, *flagWidth, *flagHeight, *flagRounds, *flagNextColor)
 }
