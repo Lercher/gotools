7a7fc8584105324b2da6eaddeeaae5a8c1084510
Martin LERCHER <martin.lercher@soprabanking.com>
2021-11-28 16:49:11 +0100

    better params

--- ell/ell.go
+++ ell/ell.go
@@ -5,19 +5,17 @@ import (
 	"log"
 )
 
-const eps = 5e-2
-
 func main() {
 	log.Println("This is ell, (c) 2021 by Martin Lercher")
 
-	printwalk(3, 120)
+	printwalk(3, 120, 5e-2)
 }
 
-func printwalk(w float64, steps int) {
-	y0 := float64(0)
-	walk00(w, steps, func(_, y float64, isZero bool) {
-		if y0 != y {
-			y0 = y
+func printwalk(w float64, steps int, epsilon float64) {
+	j0 := 0
+	walk00(w, steps, epsilon, func(_, j int, _, _, _ float64, isZero bool) {
+		if j0 != j {
+			j0 = j
 			fmt.Println()
 		}
 		if isZero {
@@ -28,20 +26,21 @@ func printwalk(w float64, steps int) {
 	})
 }
 
-func walk00(w float64, steps int, result func(x, y float64, isZero bool)) {
+func walk00(w float64, steps int, epsilon float64, result func(i, j int, x, y, val float64, isZero bool)) {
 	delta := w / float64(steps)
 	for j := 0; j < steps; j++ {
 		y := -w/2 + float64(j)*delta
 		for i := 0; i < steps; i++ {
 			x := -w/2 + float64(i)*delta
-			result(x, y, isZeroAt(x, y))
+			v, zero := isZeroAt(x, y, epsilon)
+			result(i, j, x, y, v, zero)
 		}
 	}
 }
 
-func isZeroAt(x, y float64) bool {
+func isZeroAt(x, y, epsilon float64) (float64, bool) {
 	v := f0(x, y)
-	return -eps <= v && v <= eps
+	return v, -epsilon <= v && v <= epsilon
 }
 
 func f0(x, y float64) float64 {
